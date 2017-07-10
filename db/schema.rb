05c58f3771ea5dbb30e32d586eee4dd965569224
Tatsiana Rudaya <Tatsiana_rudaya@epam.com>
2017-07-10 16:11:55 +0300

    added microposts and validate empty fields

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170707111813) do
+ActiveRecord::Schema.define(version: 20170710111802) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
